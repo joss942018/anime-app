d7e4a16377aa05277344b380dc7039a86d6efa55
Jose Alava <josevai1994@gmail.com>
2023-05-19 01:29:48 -0500

    Adding modules files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AnimeModule } from './anime/anime.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AnimeModule],
 })
 export class AppModule {}
+
